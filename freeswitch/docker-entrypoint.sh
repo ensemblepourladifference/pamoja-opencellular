61f5757195313b6a2b2e248e587bf22b6d683dcf
Omar Ramadan <omar.ramadan93@gmail.com>
2019-02-15 16:27:15 -0800

    Add FS

---
+++ freeswitch/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Source docker-entrypoint.sh:
+# https://github.com/docker-library/postgres/blob/master/9.4/docker-entrypoint.sh
+# https://github.com/kovalyshyn/docker-freeswitch/blob/vanilla/docker-entrypoint.sh
+
+if [ "$1" = 'freeswitch' ]; then
+
+    chown -R freeswitch:freeswitch /usr/local/freeswitch/
+    
+    if [ -d /docker-entrypoint.d ]; then
+        for f in /docker-entrypoint.d/*.sh; do
+            [ -f "$f" ] && . "$f"
+        done
+    fi
+    
+    exec gosu freeswitch /usr/local/freeswitch/bin/freeswitch -base /usr/local/freeswitch -u freeswitch -g freeswitch -nonat -c
+fi
+
+exec "$@"
