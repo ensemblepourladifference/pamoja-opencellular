1219dfe3417e0f63032e47aad05836098388c463
Omar Ramadan <omar.ramadan93@gmail.com>
2019-03-25 07:29:49 -0700

    cleanup docker-compose and deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "$USER deploying to $HOST..."
 echo
 
 # Copy all infrastructure files to the server
-rsync -avz -e "ssh -p 4422" -rP docker-compose* infrastructure packages freeswitch Dockerfile-freeswitch $USER@$HOST:/tmp/compose/
+rsync -avz -e "ssh -p 4422" -rP docker-compose* infrastructure packages $USER@$HOST:/tmp/compose/
 
 # Deploy the services
 ssh $USER@$HOST -p 4422 'cd /tmp/compose && docker-compose -p pamoja -f docker-compose.deps.yml -f docker-compose.yml -f docker-compose.server.yml up -d'
